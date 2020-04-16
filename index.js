8201e22b34f642bb025e9c512bf0b053afca1563
Radoslaw Polczyk <dolorismachina@gmail.com>
2020-04-16 18:53:32 +0100

    Add scaffolding

---
+++ index.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/', (req, res) => {
+  console.log(req.body.target)
+
+  res.end()
+})
+
+
+app.get('/:url', (req, res) => {
+  res.redirect('https://google.com')
+})
+
+app.listen(3000)
