2349cbaadb0d0fd1dabee47cc6d9f65fcd58b493
Radoslaw Polczyk <dolorismachina@gmail.com>
2020-04-22 21:17:01 +0100

    Redirect browser to target url

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const db = require('../db')
 const express = require('express')
 
 const router = express.Router()
@@ -8,6 +9,27 @@ router.get('/', (req, res) => {
     path: req.originalUrl
   })
 })
+
+
+router.get('/:url', async (req, res) => {
+  try {
+    const query = {
+      text: 'SELECT target from links WHERE url = $1',
+      values: [req.params.url]
+    }
+    const result = await db.query(query)  
+    if (result.rows.length === 0) {
+      res.send('Could not find url')
+      return
+    }
+
+    res.redirect(result.rows[0].target)
+  } 
+  catch (err) {
+    
+  }
+})
+
 
 router.post('/', (req, res) => {
   res.send('POST: /')
